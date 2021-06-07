a4202d060c54583ba13405ff5a4e89b621e96913
Nick Reuter <thenewimagineer@gmail.com>
2021-06-07 18:48:35 -0400

    Change LED pin to 4 and make colors solid

--- TangledLantern.ino
+++ TangledLantern.ino
@@ -1,11 +1,11 @@
 #include <Adafruit_NeoPixel.h>
 int numberOfLeds = 12;
-Adafruit_NeoPixel pixels(numberOfLeds, 6, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(numberOfLeds, 4, NEO_GRB + NEO_KHZ800);
 int generatedNumber;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  randomSeed(analogRead(0));
+  randomSeed(100);
   pixels.begin();
   pixels.show();
 }
@@ -13,7 +13,8 @@ void setup() {
 void loop() {
   generatedNumber = random(0, 15);
   for (int i = 0; i < numberOfLeds; i++) {
-    pixels.setPixelColor(i, pixels.Color(255 - generatedNumber,150 - generatedNumber, 0));
+//    pixels.setPixelColor(i, pixels.Color(255 - generatedNumber,150 - generatedNumber, 0));
+    pixels.setPixelColor(i, pixels.Color(255, 150, 0));
   }
   pixels.show();
   delay(100);
