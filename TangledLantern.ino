fc3161b92a8ae52befd5aef16574c4ffe9c84864
Nick Reuter <thenewimagineer@gmail.com>
2021-06-07 22:52:21 -0400

    Change flicker to using MOSFET and add on/off button toggle

--- TangledLantern.ino
+++ TangledLantern.ino
@@ -2,20 +2,47 @@
 int numberOfLeds = 12;
 Adafruit_NeoPixel pixels(numberOfLeds, 4, NEO_GRB + NEO_KHZ800);
 int generatedNumber;
+boolean pressed = false;
+boolean ledsOn = false;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(2, INPUT);
+  digitalWrite(3, HIGH);
   randomSeed(100);
   pixels.begin();
   pixels.show();
 }
 
 void loop() {
-  generatedNumber = random(0, 15);
-  for (int i = 0; i < numberOfLeds; i++) {
-//    pixels.setPixelColor(i, pixels.Color(255 - generatedNumber,150 - generatedNumber, 0));
-    pixels.setPixelColor(i, pixels.Color(255, 150, 0));
+  if(!pressed && digitalRead(2) == HIGH) {
+    pressed = true;
+    if(ledsOn) {
+      for (int i = 0; i < numberOfLeds; i++) {
+        pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+      }
+      pixels.show();
+      digitalWrite(9, 0);
+    }
+    else {
+      for (int i = 0; i < numberOfLeds; i++) {
+        pixels.setPixelColor(i, pixels.Color(255, 130, 0));
+      }
+      pixels.show();
+      digitalWrite(9, HIGH);
+    }
+    ledsOn = !ledsOn;
   }
-  pixels.show();
+
+  if(pressed && digitalRead(2) == LOW) {
+    pressed = false;
+  }
+
+  if(ledsOn) {
+    generatedNumber = random(200, 255);
+    analogWrite(9, generatedNumber);
+  }
   delay(100);
 }
